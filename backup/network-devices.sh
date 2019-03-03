8a16d3173dcfc80572c2376fdc546d1ba007dd77
ntv1000 <christian.ntv1000@gmail.com>
2019-03-03 16:37:05 +0100

    Move borg backup scripts out of test directory.

--- backup/network-devices.sh
+++ backup/network-devices.sh
@@ -4,35 +4,68 @@ script_dir=$(dirname $0)
 echo "Switching to $script_dir"
 cd $script_dir
 
-source ../telegraf-inputs/send-event.sh homeserver:8086 telegraf backup_started "Backup started" "Backing up network devices" "backup"
+source ../../telegraf-inputs/send-event.sh homeserver:8086 telegraf backup_started "Backup started" "Backing up network devices (borg)" "backup"
+source ./borg-prune-strats.sh
 
-destination="/mnt/network/diskstation/christian/backups/network-devices"
 
-date=$(date "+%F-%H%M%S")
 
-archer1="archer-c2-192.168.1.4-$date.bin"
-archer2="archer-c2-192.168.1.5-$date.bin"
-netgear="netgear-r7000-192.168.1.1-$date.bin"
+export BORG_REPO=/mnt/network/diskstation/christian/backups/borg-repo
+export BORG_PASSPHRASE="$1"
 
-source archer-c2.sh
-source netgear-r7000.sh
+
+single_file_backup() {
+	echo "Creating backup..."
+	borg create                         \
+		--lock-wait 43200               \
+		--verbose                       \
+		--filter AME                    \
+		--list                          \
+		--stats                         \
+		--show-rc                       \
+		--compression auto,zstd         \
+										\
+		::"$2-full-{now}"               \
+		$1
+	echo "done"
+}
+
+archer1_id="archer-c2-192.168.1.4"
+archer2_id="archer-c2-192.168.1.5"
+netgear_id="netgear-r7000-192.168.1.1"
+config="config.bin"
+
+source ../archer-c2.sh
+source ../netgear-r7000.sh
+
 
 echo "Backing up router at 192.168.1.4"
-archer-c2 "192.168.1.4" $archer1
+archer-c2 "192.168.1.4" $config
 if [[ $? == 0 ]]; then
-	mv $archer1 $destination/archer-c2-1
+	single_file_backup $config $archer1_id
+else
+	echo "Backup $archer1_id failed."
 fi
+rm -f $config
+default_prune $archer1_id
+
 
 echo "Backing up router at 192.168.1.5"
-archer-c2 "192.168.1.5" $archer2
+archer-c2 "192.168.1.5" $config
 if [[ $? == 0 ]]; then
-	mv $archer2 $destination/archer-c2-2
+	single_file_backup $config $archer2_id
+else
+	echo "Backup $archer2_id failed."
 fi
+rm -f $config
+default_prune $archer2_id
+
 
 echo "Backing up router at 192.168.1.1"
-netgear-r7000 "192.168.1.1" $netgear
+netgear-r7000 "192.168.1.1" $config
 if [[ $? == 0 ]]; then
-	mv $netgear $destination/netgear-r7000
+	single_file_backup $config $netgear_id
+else
+	echo "Backup $netgear_id failed."
 fi
-
-# TODO remove old backups
+rm -f $config
+default_prune $netgear_id
