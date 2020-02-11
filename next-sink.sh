5172c1f89b34400cd4101177f92b9f1abcfb8387
ntv1000 <christian.ntv1000@gmail.com>
2020-02-11 15:19:51 +0100

    Fix next-sink script.

--- next-sink.sh
+++ next-sink.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-CURRENT_SINK_INDEX=$(pacmd list-sinks | grep "* index: " | tr -dc '0-9')
-HIGHEST_SINK_INDEX=$(pacmd list-sinks | grep " index: " | tail -n1 | tr -dc '0-9')
+CURRENT_SINK_INDEX="$(pacmd list-sinks | grep '* index: ' | tr -dc '0-9')"
+CARDS="$(pacmd list-sinks | grep "index: " | tr -dc '0-9\n' | sort)"
+PICKNEXTCARD=1
 
-NEXT_SINK_INDEX=$((($CURRENT_SINK_INDEX + 1) % ($HIGHEST_SINK_INDEX + 1)))
+for card in $CARDS; do
+	if [ "$PICKNEXTCARD" == 1 ]; then
+		NEXT_SINK_INDEX="$card"
+		PICKNEXTCARD=0
+	fi
+	if [ "$card" == "$CURRENT_SINK_INDEX" ]; then
+		PICKNEXTCARD=1
+	fi
+done
 
-pacmd set-default-sink $NEXT_SINK_INDEX
+echo "Switching from sink $CURRENT_SINK_INDEX to sink $NEXT_SINK_INDEX."
+pacmd set-default-sink "$NEXT_SINK_INDEX"
 
-# Inputs have to be moved to the new sink
-pacmd list-sink-inputs | grep " index: " | while read input_line
+pacmd list-sink-inputs | grep index | cut -c 12- | while read -r input
 do
-	input_index=$(echo "$input_line" | tr -dc '0-9')
-	pacmd move-sink-input $input_index $NEXT_SINK_INDEX
+	echo "Moving input $input to sink $NEXT_SINK_INDEX."
+	pacmd move-sink-input "$input" "$NEXT_SINK_INDEX"
 done
