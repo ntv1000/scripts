173f9b59ee909c7e067e01e08389d3aefccd5140
ntv1000 <christian.ntv1000@gmail.com>
2021-09-25 23:46:20 +0200

    Switch to pactl to switch default sink.

--- next-sink.sh
+++ next-sink.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-CURRENT_SINK_INDEX="$(pacmd list-sinks | grep '* index: ' | tr -dc '0-9')"
-CARDS="$(pacmd list-sinks | grep "index: " | tr -dc '0-9\n' | sort)"
+CURRENT_SINK_NAME="$(pactl info | grep 'Default Sink' | awk '{print $3}')"
+CURRENT_SINK_INDEX="$(pactl list short sinks | grep "$CURRENT_SINK_NAME" | awk '{print $1}')"
+CARDS="$(pactl list short sinks | awk '{print $1}' | sort)"
 PICKNEXTCARD=1
 
 for card in $CARDS; do
@@ -15,10 +16,10 @@ for card in $CARDS; do
 done
 
 echo "Switching from sink $CURRENT_SINK_INDEX to sink $NEXT_SINK_INDEX."
-pacmd set-default-sink "$NEXT_SINK_INDEX"
+pactl set-default-sink "$NEXT_SINK_INDEX"
 
-pacmd list-sink-inputs | grep index | cut -c 12- | while read -r input
+pactl list short sink-inputs | awk '{print $1}' | while read -r input
 do
 	echo "Moving input $input to sink $NEXT_SINK_INDEX."
-	pacmd move-sink-input "$input" "$NEXT_SINK_INDEX"
+	pactl move-sink-input "$input" "$NEXT_SINK_INDEX"
 done
